80d4f72b269e76f7a01e8e07d527c8c883cead2f
Isabelle434 <79907310+Isabelle434@users.noreply.github.com>
2021-05-18 14:58:33 +0200

    Header, Footer, weitere Funktionen hinzugefügt

--- medtrack/daten.py
+++ medtrack/daten.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 import json
 
-def speichern(datei, vorname, name, geburtstag, morgen, mittag, abend, nacht, bemerkung):
+def speichern(vorname, name, geburtstag, morgen, mittag, abend, nacht, bemerkung):
+    datei = "personen.json"
     try:
         with open(datei) as open_file:
             datei_inhalt = json.load(open_file)
@@ -22,11 +24,6 @@ def speichern(datei, vorname, name, geburtstag, morgen, mittag, abend, nacht, be
         json.dump(datei_inhalt, open_file, indent=4)
 
 
-def eintrag_speichern(vorname, name, geburtstag, morgen, mittag, abend, nacht, bemerkung):
-    datei_name = "personen.json"
-    speichern(datei_name, vorname, name, geburtstag, morgen, mittag, abend, nacht, bemerkung)
-
-
 def suche(suchbegriff):
     with open("personen.json") as datei_name:
         json_as_string = datei_name.read()
@@ -36,3 +33,29 @@ def suche(suchbegriff):
         else:
             result = "no"
         return result
+
+
+def speichernlog(person, datum, morgen, mittag, abend, nacht):
+    datei = "logfile.json"
+    try:
+        with open(datei) as open_file:
+            datei_inhalt = json.load(open_file)
+    except FileNotFoundError:
+        datei_inhalt = {}
+
+    datei_inhalt[str(person)][datum] = {
+        "Morgen": morgen,
+        "Mittag": mittag,
+        "Abend": abend,
+        "Nacht": nacht
+    }
+
+    with open(datei, "w") as open_file:
+        json.dump(datei_inhalt, open_file, indent=4)
+
+def alles(datei):
+    with open(datei) as datei_name:
+        json_as_string = datei_name.read()
+        my_read_dict = json.loads(json_as_string)
+        return my_read_dict
+
--- medtrack/main.py
+++ medtrack/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+
 import daten
 
 app = Flask("medtrack")
@@ -11,26 +12,26 @@ app = Flask("medtrack")
 def suche():
     if request.method == 'POST':
         suche_person = request.form['suchbegriff']
-        return daten.suche(suche_person)
+        ergebnis = daten.suche(suche_person)
+        return render_template("suche.html", suchbegriff=suche_person, datensatz=ergebnis)
     else:
         return render_template("index.html")
 
+@app.route("/bestaetigung", methods=['Get', 'POST'])
+def bestaetigung():
+    if request.method == 'POST':
+        person = request.form['person']
+        datum = request.form['datum']
+        morgen = request.form['morgen']
+        mittag = request.form['mittag']
+        abend = request.form['abend']
+        nacht = request.form['nacht']
+        daten.speichernlog(person, datum, morgen, mittag, abend, nacht)
+        return "bestätigt"
+    else:
+        return render_template("suche.html")
 
-#@app.route("/neuereintrag/", methods=['GET', 'POST'])
-#def neuereintrag():
-#    if request.method == 'POST':
-#        vorname_person = request.form['vorname_neu']
-#        nachname_person = request.form['vorname_neu']
-#        geburtsdatum_person = request.form['geburtsdatum_neu']
-#        morgen_person = request.form['morgen_neu']
-#        mittag_person = request.form['mittag_neu']
-#        abend_person = request.form['abend_neu']
-#        nacht_person = request.form['nacht_neu']
-#        bemerkungen_person = request.form['bemerkungen_neu']
-#        return render_template("erfasst.html", vorname_neu=vorname_person, nachname_neu=nachname_person, geburtsdatum_neu=geburtsdatum_person, morgen_neu=morgen_person, mittag_neu=mittag_person, abend_neu=abend_person, nacht_neu=nacht_person, bemerkungen_neu=bemerkungen_person)
-#        vorname_person, nachname_person = daten.eintrag_speichern(vorname_person, nachname_person)
-#    else:
-#        return render_template("neuereintrag.html")
+
 
 @app.route("/neuereintrag/", methods=['GET', 'POST'])
 def neuereintrag():
@@ -43,10 +44,25 @@ def neuereintrag():
         abend = request.form['abend']
         nacht = request.form['nacht']
         bemerkung = request.form['bemerkung']
-        daten.eintrag_speichern(vorname, name, geburtstag, morgen, mittag, abend, nacht, bemerkung)
+        daten.speichern(vorname, name, geburtstag, morgen, mittag, abend, nacht, bemerkung)
         return render_template("erfasst.html", vorname=vorname, name=name, geburtstag=geburtstag, morgen=morgen, mittag=mittag, abend=abend, nacht=nacht, bemerkung=bemerkung)
     else:
         return render_template("neuereintrag.html")
+
+@app.route("/personen")
+def allepersonen():
+    datei = "personen.json"
+    personen = daten.alles(datei)
+    return render_template("personen.html", personen=personen)
+
+@app.route("/logfiles")
+def allelogs():
+    datei = "logfile.json"
+    logfiles = daten.alles(datei)
+    return render_template("logfiles.html", logfiles=logfiles)
+
+
+
 
 
 if __name__ == "__main__":
