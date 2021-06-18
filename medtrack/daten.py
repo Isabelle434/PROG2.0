2b9e423ed5164e2d508d1ee7ce60afe864674b04
Isabelle434 <79907310+Isabelle434@users.noreply.github.com>
2021-06-18 16:13:19 +0200

    improved functions

--- medtrack/daten.py
+++ medtrack/daten.py
@@ -1,5 +1,12 @@
 import json
+from plotly.offline import plot
+import plotly.express as px
 
+
+# Quelle für das Öffnen der Datei (json.load, usw.) sind die Vorlesungsunterlagen.
+
+
+# Neuer Eintrag in JSON Datei speichern
 def speichern(vorname, name, geburtstag, morgen, mittag, abend, nacht, bemerkung):
     datei = "personen.json"
     try:
@@ -19,11 +26,12 @@ def speichern(vorname, name, geburtstag, morgen, mittag, abend, nacht, bemerkung
         "Bemerkungen": bemerkung
     }
 
-
     with open(datei, "w") as open_file:
         json.dump(datei_inhalt, open_file, indent=4)
 
-
+# Suchfunktion, welche die Keys des Dictionary nach dem Suchbegriff durchsucht
+# Wenn dieser exakt gefunden wird, wird das entsprechende Dictionary als Resultat zurückgegeben.
+# Ansonsten wird die Meldung, dass kein Eintrag gefunden wurde, zurückgegeben.
 def suche(suchbegriff):
     with open("personen.json") as datei_name:
         json_as_string = datei_name.read()
@@ -31,10 +39,10 @@ def suche(suchbegriff):
         if suchbegriff in my_read_dict.keys():
             result = my_read_dict[suchbegriff]
         else:
-            result = "no"
+            result = "Nein"
         return result
 
-
+# Speichern eines Tagesabschlusses in der JSON Datei
 def speichernlog(person, datum, morgen, mittag, abend, nacht):
     datei = "logfile.json"
     try:
@@ -62,15 +70,69 @@ def speichernlog(person, datum, morgen, mittag, abend, nacht):
     with open(datei, "w") as open_file:
         json.dump(datei_inhalt, open_file, indent=4)
 
+# Laden des kompletten Inhaltes einer Datei.
+# Wird verwendet für die Approutes "Allepersonen" und "Allelogs".
 def alles(datei):
     with open(datei) as datei_name:
         json_as_string = datei_name.read()
         my_read_dict = json.loads(json_as_string)
         return my_read_dict
 
-
+# Aufrufen eines einzelnen Datensatzes (verschachteltes Dictionary) aus der JSON Datei.
+# Wird verwendet in der Approute "Allepersonen" und "Allelogs", wenn bei einer Person auf "Logfiles" geklickt wird.
 def datensatz(datei, person):
     with open(datei) as datei_name:
         json_as_string = datei_name.read()
         my_read_dict = json.loads(json_as_string)
         return my_read_dict[person]
+
+#Quelle: https://plotly.com/python/pie-charts/ und Vorlesungsunterlagen aus Programmieren 2
+#Visualisiert aus Ja und Nein Werten ein Kuchendiagramm.
+def piechart(counterja, counternein, titel):
+    dfmo = {
+    "Names": [
+        "Ja",
+        "Nein"
+        ],
+    "Values": [
+        counterja,
+        counternein
+    ]
+    }
+    fig = px.pie(dfmo, names="Names", values="Values", title=titel, color_discrete_sequence=["green", "red"])
+    fig.update_layout(margin=dict(t=0, b=0, l=0, r=0))
+    div = plot(fig, output_type="div")
+    return div
+
+def df(datei, person):
+    with open(datei) as datei_name:
+        json_as_string = datei_name.read()
+        my_read_dict = json.loads(json_as_string)
+        countermoja = 0
+        countermonein = 0
+        countermija = 0
+        counterminein = 0
+        counterabja = 0
+        counterabnein = 0
+        counternaja = 0
+        counternanein = 0
+        dict = my_read_dict[person]
+        for key, value in dict.items():
+            if value["Morgen"] == "Ja":
+                countermoja = countermoja +1
+            else:
+                countermonein = countermonein +1
+            if value["Mittag"] == "Ja":
+                countermija = countermija +1
+            else:
+                counterminein = counterminein +1
+            if value["Abend"] == "Ja":
+                counterabja = counterabja +1
+            else:
+                counterabnein = counterabnein +1
+            if value["Nacht"] == "Ja":
+                counternaja = counternaja +1
+            else:
+                counternanein = counternanein +1
+        return countermoja, countermonein, countermija, counterminein, counterabja, counterabnein,counternaja, counternanein
+
--- medtrack/main.py
+++ medtrack/main.py
@@ -7,16 +7,20 @@ import daten
 
 app = Flask("medtrack")
 
-
+# Bekommt ein Suchbegriff aus dem Formular der Seite index.html und ruft die suche Funktion auf (Datei Daten.py)
 @app.route("/index/", methods=['GET', 'POST'])
 def suche():
     if request.method == 'POST':
         suche_person = request.form['suchbegriff']
         ergebnis = daten.suche(suche_person)
-        return render_template("suche.html", suchbegriff=suche_person, datensatz=ergebnis)
+        if ergebnis == "Nein":
+            return render_template("index.html", suchfunktion="Unter diesem Namen gibt es keinen Eintrag. Bitte überprüfe die Schreibweise.")
+        else:
+            return render_template("suche.html", suchbegriff=suche_person, datensatz=ergebnis)
     else:
         return render_template("index.html")
 
+# Bekommt die Daten aus dem Formular der Seite abschluss.html und ruft die speichernlog Funktion auf (Datei Daten.py).
 @app.route("/abschluss", methods=['GET', 'POST'])
 def abschluss():
     if request.method == 'POST':
@@ -43,8 +47,7 @@ def abschluss():
     else:
         return render_template("abschluss.html")
 
-
-
+# Bekommt die Daten aus dem Formular und ruft die Speichern Funktion auf (Datei Daten.py).
 @app.route("/neuereintrag/", methods=['GET', 'POST'])
 def neuereintrag():
     if request.method == 'POST':
@@ -61,32 +64,43 @@ def neuereintrag():
     else:
         return render_template("neuereintrag.html")
 
+# Bekommt vom Formular bzw. Button den value einer Person und ruft die Datensatz Funktion auf (Datei Daten.py)
+# Danach wird logfile.html wiedergegeben, um die Einträge zu der aufgerufenen Person anzuzeigen.
 @app.route("/personen", methods=['GET', 'POST'])
 def allepersonen():
     if request.method == 'POST':
-        #button = request.form['button']
-        #if button == "logfileperson":
         person = request.form['logfileperson']
         datei = "logfile.json"
         logfile = daten.datensatz(datei, person)
-        return render_template("logfile.html", person=person, logfile=logfile)
-        #if button == "tagesabschluss":
-        #    person = request.form['tagesabschluss']
-        #    return render_template("abschluss.html", person=person)
+        countermoja, countermonein, countermija, counterminein, counterabja, counterabnein,counternaja, counternanein = daten.df(datei, person)
+        divmorgen = daten.piechart(countermoja, countermonein, "Morgens")
+        divmittag = daten.piechart(countermija, counterminein, "Mittags")
+        divabend = daten.piechart(counterabja, counterabnein, "Abends")
+        divnacht = daten.piechart(counternaja, counternanein, "Nacht")
+        return render_template("logfile.html", person=person, logfile=logfile, piechartmorgens=divmorgen, piechartmittags=divmittag, piechartabends=divabend, piechartnachts=divnacht)
     else:
         datei = "personen.json"
         personen = daten.alles(datei)
         return render_template("personen.html", personen=personen)
 
-
-@app.route("/logfiles")
+# Es werden alle Einträge der Datei logfile.json angezeigt.
+# Es kann via Button zur Ansicht der Logfiles einer einzelnen Person gewechselt werden (Seite logfile.html).
+@app.route("/logfiles", methods=['GET', 'POST'])
 def allelogs():
-    datei = "logfile.json"
-    logfiles = daten.alles(datei)
-    return render_template("logfiles.html", logfiles=logfiles)
-
-
-
+    if request.method == 'POST':
+        person = request.form['logfileperson']
+        datei = "logfile.json"
+        logfile = daten.datensatz(datei, person)
+        countermoja, countermonein, countermija, counterminein, counterabja, counterabnein, counternaja, counternanein = daten.df(datei, person)
+        divmorgen = daten.piechart(countermoja, countermonein, "Morgens")
+        divmittag = daten.piechart(countermija, counterminein, "Mittags")
+        divabend = daten.piechart(counterabja, counterabnein, "Abends")
+        divnacht = daten.piechart(counternaja, counternanein, "Nacht")
+        return render_template("logfile.html", person=person, logfile=logfile, piechartmorgens=divmorgen, piechartmittags=divmittag, piechartabends=divabend, piechartnachts=divnacht)
+    else:
+        datei = "logfile.json"
+        logfiles = daten.alles(datei)
+        return render_template("logfiles.html", logfiles=logfiles)
 
 
 if __name__ == "__main__":
